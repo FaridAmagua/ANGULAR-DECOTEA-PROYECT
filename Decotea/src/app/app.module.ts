abb5fce9e71185c40967d964abbe418d55e4eaeb
farid faridamagua <faridamagua@live.com>
2023-07-03 10:05:43 +0200

    nosotros component

--- Decotea/src/app/app.module.ts
+++ Decotea/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NosotrosComponent } from './pages/nosotros/nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    NosotrosComponent,
   ],
   imports: [
     BrowserModule,
