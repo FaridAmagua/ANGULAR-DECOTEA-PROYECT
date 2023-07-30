d47267a8695a4e8bab3d3a9d0b0e722229e1c7ae
farid faridamagua <faridamagua@live.com>
2023-07-30 14:58:16 +0200

    deleted nosotros

--- Decotea/src/app/app-routing.module.ts
+++ Decotea/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 
 
 
@@ -11,11 +10,7 @@ const routes: Routes = [
     component:HomeComponent,
     pathMatch:'full'
   },
-  {
-    path:'nosotros',
-    component:NosotrosComponent,
-    pathMatch:'full'
-  },
+ 
 ];
 
 @NgModule({
--- Decotea/src/app/app.module.ts
+++ Decotea/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 
@@ -14,7 +13,6 @@ import { ContactoComponent } from './pages/contacto/contacto.component';
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    NosotrosComponent,
     ProductosComponent,
     ContactoComponent,
   ],
