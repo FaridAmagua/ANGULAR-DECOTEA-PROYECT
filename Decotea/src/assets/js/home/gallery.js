a1a9a7180cecfcc47e14614961e7986779d2802c
farid faridamagua <faridamagua@live.com>
2023-08-24 15:32:49 +0200

    commit 2023 08

--- Decotea/src/assets/js/home/gallery.js
+++ Decotea/src/assets/js/home/gallery.js
@@ -1,38 +1,30 @@
+// Selecciona el elemento del carrusel
+const carouselElement = document.querySelector('.carousel__lista');
 
-    new Glider(document.querySelector('.carousel__lista'), {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        draggable: true,
-        dots: '.carousel__indicadores',
-        arrows: {
-          prev: '.carousel__anterior',
-          next: '.carousel__siguiente'
-        },
-        responsive: [
-          {
-            // screens greater than >= 775px
-            breakpoint: 300,
-            settings: {
-            // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 1,
-            slidesToScroll: 1
-            }
-          },
-          {
-            // screens greater than >= 775px
-            breakpoint: 450,
-            settings: {
-            // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 3,
-            slidesToScroll: 3
-            }
-          },{
-            // screens greater than >= 1024px
-            breakpoint: 800,
-            settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4
-            }
-          }
-        ]
-      });
+// Configuración del carrusel Glider
+const glider = new Glider(carouselElement, {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  dots: '.carousel__indicadores',
+  arrows: {
+    prev: '.carousel__anterior',
+    next: '.carousel__siguiente'
+  },
+});
+
+// Función para pasar a la siguiente imagen automáticamente
+function autoSlide() {
+  const currentIndex = glider.getCurrentSlide();
+  const nextIndex = (currentIndex + 1) % glider.slides.length;
+  glider.scrollItem(nextIndex);
+}
+
+// Intervalo de tiempo para cambiar las imágenes cada 3 segundos (ajusta según lo desees)
+let interval = setInterval(autoSlide, 3000); // Cambia de imagen cada 3 segundos
+
+// Detener el intervalo cuando el mouse está sobre el carrusel
+carouselElement.addEventListener('mouseover', () => clearInterval(interval));
+
+// Reanudar el intervalo cuando el mouse sale del carrusel
+carouselElement.addEventListener('mouseout', () => interval = setInterval(autoSlide, 3000));
